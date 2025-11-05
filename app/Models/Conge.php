5c901d0a631f1ec93480e82b8135178b1e501eef
Francohedi1999 <ramarotafika1999@gmail.com>
2025-11-05 16:52:17 +0300

    Create Conge.php

---
+++ app/Models/Conge.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Conge extends Model
+{
+    protected $table = 'Employes';
+    
+    protected $fillable = [
+        'employee_id' ,
+        'date_debut' ,
+        'date_fin' ,
+        'type_conge' ,
+        'commentaire' ,
+    ];
+
+    public function employe(): BelongsTo
+    {
+        return $this->belongsTo(Employe::class, 'employee_id', 'ID');
+    }
+}
