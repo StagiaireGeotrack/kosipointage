7c6a59c7c92fdb1fcb88e1a24d42edfe7a7cc36f
Francohedi1999 <ramarotafika1999@gmail.com>
2025-11-06 10:27:30 +0300

    Update ProfileUpdateRequest.php

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -1,4 +1,5 @@
 <?php
+// app/Http/Requests/ProfileUpdateRequest.php
 
 namespace App\Http\Requests;
 
@@ -8,6 +9,14 @@ use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -25,6 +34,27 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => __('Le nom est obligatoire.'),
+            'name.string' => __('Le nom doit être une chaîne de caractères.'),
+            'name.max' => __('Le nom ne peut pas dépasser :max caractères.'),
+            
+            'email.required' => __('L\'adresse e-mail est obligatoire.'),
+            'email.string' => __('L\'adresse e-mail doit être une chaîne de caractères.'),
+            'email.lowercase' => __('L\'adresse e-mail doit être en minuscules.'),
+            'email.email' => __('L\'adresse e-mail doit être valide.'),
+            'email.max' => __('L\'adresse e-mail ne peut pas dépasser :max caractères.'),
+            'email.unique' => __('Cette adresse e-mail est déjà utilisée.'),
         ];
     }
 }
