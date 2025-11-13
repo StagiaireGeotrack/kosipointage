495ca1dcf2f87f1b756148a9c1f9fa0fd7e4cfcf
Francohedi1999 <ramarotafika1999@gmail.com>
2025-11-13 12:05:45 +0300

    modal 1

--- resources/views/administration/index.blade.php
+++ resources/views/administration/index.blade.php
@@ -146,15 +146,15 @@
                                                         <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                                                     </svg>
                                                 </a>
-                                                <form action="{{ route('administrateurs.destroy', $admin->ID) }}" method="POST" class="d-inline">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-link text-danger p-0 border-0" onclick="return confirm('{{ __('Voulez-vous vraiment supprimer cet administrateur ?') }}')" title="Supprimer">
-                                                        <svg class="bi" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
-                                                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                                        </svg>
-                                                    </button>
-                                                </form>
+                                                <button type="button" class="btn btn-link text-danger p-0 border-0" 
+                                                    onclick="setDeleteAction('{{ route('administrateurs.destroy', $admin->ID) }}', '{{ $admin->Identifiant_email }}')" 
+                                                    data-bs-toggle="modal" 
+                                                    data-bs-target="#deleteModal" 
+                                                    title="Supprimer">
+                                                    <svg class="bi" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
+                                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </button>
                                             @else
                                                 <a href="{{ route('profile.edit') }}" class="text-secondary" title="Modifier">
                                                     <svg class="bi" width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
@@ -181,6 +181,32 @@
                     </table>
                 </div>
                 
+                
+
+                <!-- Modal de confirmation (à placer en dehors de la boucle) -->
+                <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModalLabel">{{ __('Confirmation de suppression') }}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>{{ __('Voulez-vous vraiment supprimer cet administrateur ?') }}</p>
+                                <p class="fw-bold" id="siegeName"></p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Annuler') }}</button>
+                                <form id="deleteForm" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">{{ __('Supprimer') }}</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <!-- Pagination -->
                 <div class="mt-1">
                     {{ $administrateurs->links("pagination.custom") }}
@@ -188,4 +214,16 @@
             </div>
         </div>
     </div>
+
+    @push("scripts")
+
+        <script>
+        function setDeleteAction(url, name) {
+            document.getElementById('deleteForm').action = url;
+            document.getElementById('siegeName').textContent = name;
+        }
+        </script>
+        
+    @endpush
+
 </x-app-layout>
