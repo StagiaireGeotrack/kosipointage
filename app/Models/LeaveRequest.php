81e7394c702002943b2cbaefafb9e64875cd4188
StagiaireGeotrack <stagiaire@geotrack-solutions.net>
2026-08-26 13:54:14 +0100

    Update LeaveRequest.php

--- app/Models/LeaveRequest.php
+++ app/Models/LeaveRequest.php
@@ -22,7 +22,7 @@ class LeaveRequest extends Model
         'duration',
         'status',
         'reason',
-        'submitted_at',
+        submitted_at?
         'comment',
         'attachment_path',
         'approved_by',
@@ -36,11 +36,9 @@ class LeaveRequest extends Model
 
     protected $casts = [
         'start_date' => 'date',
-        'end_date' => 'date2',
+        'end_date' => 'date',
         'approved_at' => 'datetime',
         'rejected_at' => 'datetime',
-        'submitted_at' => 'datetime',
-
         'duration' => 'decimal:2',
     ];
 
